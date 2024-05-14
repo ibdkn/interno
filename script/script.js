69259ccf680514925ec1de1059271241c12a6d2b
ibdkn <ibudiakina@gmail.com>
2024-05-14 13:49:19 +0300

    changed script.js

--- script/script.js
+++ script/script.js
@@ -1,30 +1,34 @@
 // Search
-const searchButton = document.getElementById('search-button');
-const closeSearchButton = document.getElementById('close-search-button');
-const overlay = document.getElementById('overlay');
+const searchButton = document.getElementById("search-button");
+const closeSearchButton = document.getElementById("close-search-button");
+const overlay = document.getElementById("overlay");
 
-searchButton.addEventListener('click', function() {
-    overlay.style.top = '0';
+searchButton.addEventListener("click", function() {
+    overlay.style.top = "0";
 });
 
-closeSearchButton.addEventListener('click', function() {
-    overlay.style.top = '-100vh';
+closeSearchButton.addEventListener("click", function() {
+    overlay.style.top = "-100vh";
 });
 
 
 // Menu
-function openModalMenu() {
-  document.getElementById("overlayMenu").style.top = "0px";
-}
-function closeModalMenu() {
-  document.getElementById("overlayMenu").style.top = "-100vh";
-}
+const openModalMenuButton = document.getElementById("open-modal-menu");
+const closeModalMenuButton = document.getElementById("close-modal-menu");
+const overlayMenu = document.getElementById("overlay-menu");
+
+openModalMenuButton.addEventListener('click', function () {
+    overlayMenu.style.top = "0px";
+})
+closeModalMenuButton.addEventListener('click', function () {
+    overlayMenu.style.top = "-100vh";
+})
 
 // Swiper
 var swiper = new Swiper(".swiper-feedback", {
     navigation: {
-      nextEl: ".swiper-button-next",
-      prevEl: ".swiper-button-prev",
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
     },
     effect: 'coverflow',
     coverflowEffect: {
